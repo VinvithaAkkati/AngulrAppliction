c9e35dbf80d539263c248944f1a6d3bcda1c23b6
VinvithaAkkati <vinvitha.akkati@gmail.com>
2026-02-08 16:13:04 -0600

    Product Binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,13 +10,21 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductListComponent {
   product = {
-  name: 'iPhone 18',
+  name: 'iPhone 18 pro max',
   price: 999,
   color: 'black',
   discount: 8.5,
-  instock: 0
+  instock: 10,
+  pImage: './assets/Images/iphone.png',
+  Owner: 'Vinvitha',
+  flag: true
+  //isOutOfStock: this.instock > 0 ? true : false,
+  
   }
-
+  
+  isOutOfStock() {
+    return this.product.instock > 0 ? false : true;
+  }
   getDiscountedPrice() {
     return (this.product.price - ((this.product.price * this.product.discount)/100))
   }
